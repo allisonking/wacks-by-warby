b89afe6dc2612cb12fe62620f55bd068af520424
Allison King <allisonjuliaking@gmail.com>
2021-03-20 10:03:08 -0400

    initial pytest scaffolding

---
+++ tests/conftest.py
@@ -0,0 +1,15 @@
+import pytest
+import requests
+
+
+@pytest.fixture
+def example_inventory():
+    return []
+
+
+@pytest.fixture(autouse=True)
+def disable_network_calls(monkeypatch):
+    def stunted_get():
+        raise RuntimeError("Network access not allowed during testing!")
+
+    monkeypatch.setattr(requests, "get", lambda *args, **kwargs: stunted_get())
