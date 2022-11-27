155925d5d4bd85da2e522ca00d393842c7ad93d8
Allison King <allisonjuliaking@gmail.com>
2022-11-27 13:53:51 -0500

    Update README

--- src/wacks4shop/shift4shop.py
+++ src/wacks4shop/shift4shop.py
@@ -9,7 +9,7 @@ logger = logging.getLogger("shift4shop")
 class Shift4Shop:
     def __init__(self, debug=False) -> None:
         self.key = os.getenv("SHIFT4SHOP_PRIVATE_KEY")
-        self.shop_token = os.getenv("SHIFT4SHOP_WICKSBYWERBY_TOKEN")
+        self.shop_token = os.getenv("SHIFT4SHOP_SHOP_TOKEN")
         self.debug = debug
 
     def get_inventory_state(self) -> dict[str, Inventory]:
