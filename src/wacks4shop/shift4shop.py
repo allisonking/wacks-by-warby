cf33650441f3544dadc686c6c08a33c08b07b76b
Allison King <allisonjuliaking@gmail.com>
2022-11-27 18:09:25 -0500

    Add docstring

--- src/wacks4shop/shift4shop.py
+++ src/wacks4shop/shift4shop.py
@@ -14,9 +14,16 @@ class Shift4Shop:
         self.debug = debug
 
     def determine_sales(self, timestamp: Optional[str]) -> list[Sale]:
+        """
+        Query the Shift4Shop API for all orders since the given timestamp. Then transform these orders
+        into a list of Sale objects
+        """
         # TODO
         return []
 
     def get_num_sales(self) -> int:
+        """
+        Get the current total number of sales by querying the Shift4Shop API
+        """
         # TODO
         return 0
