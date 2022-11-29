d65c3a35bc20e856c97a6430b47cfdfc569736de
Allison King <allisonjuliaking@gmail.com>
2022-11-29 13:05:58 -0500

    Fix color

--- src/wacksbywarby/werbies.py
+++ src/wacksbywarby/werbies.py
@@ -15,4 +15,4 @@ class Werbies:
         except FileNotFoundError:
             id_to_data = {}
         data = id_to_data.get(listing_id)
-        return Werby(**data) if data else None
+        return Werby(color=None, **data) if data else None
