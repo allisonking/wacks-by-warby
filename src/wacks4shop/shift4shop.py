fff3169d89671923e0a46828bbd9b43494fa3da6
Jimmy Zeng <jzengg@gmail.com>
2022-12-31 11:28:07 -0500

    fix bug with orderstatus filtering

--- src/wacks4shop/shift4shop.py
+++ src/wacks4shop/shift4shop.py
@@ -7,6 +7,7 @@ import requests
 
 from wacksbywarby.constants import SHIFT4SHOP_ORDER_DATE_FORMAT, SHIFT4SHOP_TIME_FORMAT
 from wacksbywarby.models import Sale
+from dotenv import load_dotenv
 
 logger = logging.getLogger("shift4shop")
 
@@ -25,7 +26,7 @@ From @rchhay:
 9 = Custom 2 (not being used i think)
 10 = Custom 3 (also not used)
 """
-ORDER_STATUSES = [1, 2, 3, 4, 5, 6, 8, 9, 10]
+INCOMPLETE_ORDER_STATUS = 7
 
 
 class Shift4Shop:
@@ -60,27 +61,40 @@ class Shift4Shop:
     def determine_sales(self, timestamp: Optional[str]) -> list[Sale]:
         """
         Query the Shift4Shop API for all orders since the given timestamp. Then transform these orders
-        into a list of Sale objects
+        into a list of Sale objects. Manually dedupe incomplete orders from all orders using
+        order id as the API doesn't support filtering by multiple order statuses.
         """
-        params: dict = {"orderstatus": ORDER_STATUSES}
+        params: dict = {}
         if timestamp:
             params["datestart"] = timestamp
-        response = self._request_orders(params)
+        all_orders_response = self._request_orders(params)
 
-        if not response.ok:
+        incomplete_orders_params: dict = {"orderstatus": 7}
+        if timestamp:
+            incomplete_orders_params["datestart"] = timestamp
+        incomplete_orders_response = self._request_orders(incomplete_orders_params)
+        incomplete_order_ids = {order["OrderID"] for order in incomplete_orders_response.json()}
+        completed_orders = [order for order in all_orders_response.json() if order["OrderID"] not in incomplete_order_ids]
+
+        if not all_orders_response.ok or not incomplete_orders_response.ok:
             # when there are no new orders, this will 404. this will happen a lot, so we
             # really only need to log when it isn't a 404
-            if response.status_code != 404:
+            if all_orders_response.status_code != 404:
                 logger.error(
-                    "Something has gone wrong! %s: %s",
-                    response.status_code,
-                    response.content,
+                    "Something has gone wrong with getting all orders! %s: %s",
+                    all_orders_response.status_code,
+                    all_orders_response.content,
+                )
+            if incomplete_orders_response.status_code != 404:
+                logger.error(
+                    "Something has gone wrong with getting incomplete orders! %s: %s",
+                    incomplete_orders_response.status_code,
+                    incomplete_orders_response.content,
                 )
             return []
 
-        orders = response.json()
         sales: list[Sale] = []
-        for order in orders:
+        for order in completed_orders:
             order_date_str = order["OrderDate"]
             order_date = datetime.strptime(order_date_str, SHIFT4SHOP_ORDER_DATE_FORMAT)
             if timestamp and order_date == datetime.strptime(
@@ -102,12 +116,36 @@ class Shift4Shop:
 
     def get_num_sales(self) -> int:
         """
-        Get the current total number of sales by querying the Shift4Shop API
+        Get the current total number of sales by querying the Shift4Shop API.
+        The API only supports filtering by 1 order status at a time so get the
+        completed orders by taking the difference between all the orders without
+        any filtering and the number of orders in the "not completed" status.
+        Additionally, there's a default "limit" applied (10) if you don't send that param
+        and an upper bound to how many orders can be returned at once so this is just
+        a stopgap solution.
+
+        Ultimately, we probably need to store previous num sales in the DB and use datestart
+        or invoicenumberstart in the future.
         """
-        params = {
-            "orderstatus": ORDER_STATUSES,
-            # count the number of rows only
-            "countonly": 1,
-        }
-        response = self._request_orders(params)
-        return response.json()["TotalCount"]
+        not_completed_orders_response = self._request_orders({
+            "orderstatus": INCOMPLETE_ORDER_STATUS,
+            "limit": 300
+        })
+        num_not_completed_orders = 0
+        for order in not_completed_orders_response.json():
+            num_not_completed_orders += len(order["OrderItemList"])
+
+        total_orders_response = self._request_orders({"limit": 300})
+        num_total_orders = 0
+        for order in total_orders_response.json():
+            num_total_orders += len(order["OrderItemList"])
+
+        num_sales = num_total_orders - num_not_completed_orders
+        return num_sales
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    shift = Shift4Shop(debug=True)
+    response = shift.get_num_sales()
+    print(response)
